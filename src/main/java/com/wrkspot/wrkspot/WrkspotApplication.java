faf9375191aea75828b92ca5ec3f44a1093a8ff5
AravindhanMurugan <55662965+AravindhanMurugan@users.noreply.github.com>
2025-03-19 07:56:31 +0530

    code commit
    push code changes

---
+++ src/main/java/com/wrkspot/wrkspot/WrkspotApplication.java
@@ -0,0 +1,13 @@
+package com.wrkspot.wrkspot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WrkspotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WrkspotApplication.class, args);
+	}
+
+}
